5f464d3f632a46cb117e1b68a1a7e3c025499eb2
podwieczorek <paulina.wieczorek555@gmail.com>
2024-08-03 18:50:23 +0200

    Modify bit flipping decoder
    Modified bf decoder by adding different bit threshold for every message bit
    in order to improve decoding performance
    
    Added more h matrices for testing purpose

--- bf_decoder.py
+++ bf_decoder.py
@@ -1,13 +1,18 @@
+"""
+Gallager's bit-flipping hard-decision decoder
+'particularly simple but is applicable only to the BSC at rates far below channel capacity'
+Algorithm described in 'Low-Density Parity-Check Codes', R. G. Gallager, 1962
+"""
+
 import numpy as np
 
-# todo test!
 
-
-def calculate_failed_parity_checks_per_bit(h_alist, n, s):
-    f = np.zeros(n)
+#  for every message bit calculate in how many failed parity checks the message bit was involved
+def calculate_failed_parity_checks(h_alist, n, s):
+    f = np.zeros(n, dtype=int)
+    alist_offset = 4
     for i in range(n):
-        mi = h_alist[i + 4]
-        for j in mi:
+        for j in h_alist[i + alist_offset]:
             if j == 0:
                 break
             else:
@@ -16,27 +21,34 @@ def calculate_failed_parity_checks_per_bit(h_alist, n, s):
     return f
 
 
-def decode(h, h_alist, received_msg):
+# flipping threshold is half of the parity checks:
+# if more than half bit's parity checks fail, the bit should be flipped
+def calculate_flipping_thresholds(h):
+    flipping_thresholds = h.sum(axis=0)
+    flipping_thresholds //= 2
+    return flipping_thresholds
 
-    max_num_of_iteration = 10  # different for every code
-    flipping_threshold = 1  # different for every code
+
+def decode(h, h_alist, received_msg):
     m, n = np.shape(h)
+
+    # max_num_of_iteration and flipping_thresholds may be different for every code
+    max_num_of_iteration = 100
+    flipping_thresholds = calculate_flipping_thresholds(h)
 
     iteration = 0
     while iteration < max_num_of_iteration:
-        # calculate syndrome
+        # calculate syndrome, if it is a zero vector, the message was decoded successfully
         s = (received_msg @ np.transpose(h)) % 2
-        # message decoded successfully
         if not s.any():
             break
 
-        #  for every message bit calculate in how many failed parity checks the message bit was involved
-        f = calculate_failed_parity_checks_per_bit(h_alist, n, s)
-
-        # flip bits involved in failed parity checks
+        # compute parity checks and flip the bits
+        f = calculate_failed_parity_checks(h_alist, n, s)
         for i, fi in enumerate(f):
-            # todo maybe flip the bit for the biggest fi?
-            if fi >= flipping_threshold:
+            if fi > flipping_thresholds[i]:
                 received_msg[i] = 1 - received_msg[i]  # flip bit
+
         iteration += 1
+
     return received_msg
