6d503cb97f6bc873506cbe4663e3e8db256d2a38
podwieczorek <paulina.wieczorek555@gmail.com>
2024-10-27 17:13:04 +0100

    Change many files at once (to gap converter)

--- utils/to_gap_array.py
+++ utils/to_gap_array.py
@@ -1,19 +1,36 @@
 import argparse
 import os
+import glob
 import warnings
 
 
-def convert_to_array(file, path):
-    file_path = os.path.join(path, 'gap_array.txt')
+def convert_to_array(path):
+    # create directory for converted files
+    new_dir = os.path.join(path, 'GapFormat')
+    if not os.path.isdir(new_dir):
+        os.mkdir(new_dir)
+
+    # convert all .alist files to .txt files and save it in created  directory
+    for txt_file in glob.glob(os.path.join(path, '*.txt')):
+        with open(txt_file, 'r') as file:
+            file_name = txt_file.split('/')[-1][:-4] + '_gap.txt'
+            new_file_path = os.path.join(new_dir, file_name)
+            convert_one_file_to_array(file, new_file_path)
+
+
+def convert_one_file_to_array(file, new_file_path):
+    if os.path.isdir(new_file_path):
+        new_file_path = os.path.join(new_file_path, 'h_gap.txt')
+
     content = file.readlines()
-    with open(file_path, 'w') as new_file:
+    with open(new_file_path, 'w') as new_file:
         row_number = len(content)
         new_file.write('[')
         for row_index, row in enumerate(content):
             new_file.write('[')
             new_file.write(row.replace(' ', ', '))
             new_file.write(']')
-            if row_index != row_number-1:
+            if row_index != row_number - 1:
                 new_file.write(', ')
         new_file.write(']')
 
@@ -24,13 +41,14 @@ def main():
     parser.add_argument('-p', '--path', help='Path to the .txt file', required=True)
     path = vars(parser.parse_args())['path']
 
-    # check if given file exists
-    if not os.path.isfile(path):
-        warnings.warn(f"File {path} does not exist, check the path!")
+    if os.path.isfile(path):
+        with open(path, 'r') as file:
+            convert_one_file_to_array(file, os.path.dirname(path))
+    elif os.path.isdir(path):
+        convert_to_array(path)
+    else:
+        warnings.warn(f"{path} does not exist, check if directory or file exists!")
         return
-
-    with open(path, 'r') as file:
-        convert_to_array(file, os.path.dirname(path))
 
 
 if __name__ == "__main__":
